50aa395d41585ce06651892c89a7560a33872610
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-10 00:05:54 +0000

    js linting

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ let day
 function checkDiscount () {
   age = document.getElementById('age').value
   day = document.getElementById('day').value
-  
-// check if requirements met
+
   if ((day.toLowerCase() === 'tuesday' || day.toLowerCase() === 'thursday') && (age > 12 && age < 21)) {
       document.getElementById('answer').innerHTML = 'You are eligible for a discount'
-  } 
+  }
   else {
     document.getElementById('answer').innerHTML = 'You are ineligible for a discount'
   } 
-} 
+}
